4ab611e922682d90c47afc972eee8e08ccabb75d
Kalam Ahmmed <0057kalamahmmed@gmail.com>
2026-01-10 23:46:34 +0600

    refactor: update MongoDB connection logic and improve error handling

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,12 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB Connected 🚀");
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1); // exit process with failure
+    console.error("Mongo error:", error.message);
+    process.exit(1);
   }
 };
 
